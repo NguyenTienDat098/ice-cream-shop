f096874ccedcbefd58dc48343db311708b3cc024
NguyenTienDat098 <dat61222@gmail.com>
2022-08-20 20:10:47 +0700

    config db

--- src/configs/db/config.js
+++ src/configs/db/config.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const url = 'mongodb + srv://NguyenTienDat098:hetpass123@cluster0.tiqp7q4.mongodb.net/my_ice_cream_database?retryWrites=true&w=majority'
+const url = `mongodb+srv://NguyenTienDat098:hetpass123@cluster0.tiqp7q4.mongodb.net/my_ice_cream_database?retryWrites=true&w=majority`
 async function connect() {
   try {
     await mongoose.connect(url, {
