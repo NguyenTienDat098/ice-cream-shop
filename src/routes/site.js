3662fbf8c789456ec683d4018b74ab50ffd515dc
NguyenTienDat098 <dat61222@gmail.com>
2022-08-21 15:18:14 +0700

    add new routes in site.js file

--- src/routes/site.js
+++ src/routes/site.js
@@ -3,10 +3,12 @@ const express = require('express');
 const router = express.Router();
 const { userInfor } = require('../app/models/Users');
 
-router.get('/customer-orders', userInfor, siteController.customerOrder);
+router.post('/order-information/:code/edit', userInfor, siteController.HandleEditCustomerOrder);
+router.get('/order-information/:code/edit', userInfor, siteController.editCustomerOrder);
 router.get('/order-information/:code', userInfor, siteController.getInforOrder);
 router.post('/order/checkout', userInfor, siteController.saveBill);
 router.get('/order/checkout', userInfor, siteController.checkout);
+router.get('/customer-orders', userInfor, siteController.customerOrder);
 router.post('/order', userInfor, siteController.saveProductOrder);
 router.get('/order', userInfor, siteController.orderProduct);
 router.get('/flavors', siteController.flavors);
